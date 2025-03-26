1adf4f0cbc8a8026d79351da7dc616e4294044d3
Filipe <Filipecasagrande1@gmail.com>
2025-03-25 21:16:10 -0300

    Teste1

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -2,43 +2,83 @@
 {
     internal class Program
     {
-        //versão 2.0: exibir bapavra oculta com traços
+        //versão implementar verificação de dados se esta na palavra
         static void Main(string[] args)
         {
             while (true)
             {
+                string palavrasecreta = "uva";
 
-                string palavrasecreta = "MELANCIA";
+                char[] letrasencontradas = new char[palavrasecreta.Length];
 
-                char[] letrasencontradas = new char[palavrasecreta.Length]; 
-
-                for(int caractere =0; caractere < letrasencontradas.Length; caractere++)
+                for (int caractere = 0; caractere < letrasencontradas.Length; caractere++)
                 {
                     letrasencontradas[caractere] = '_';
                 }
 
-                string dicadapalavra = String.Join(" ", letrasencontradas)
-                
-                Console.Clear();
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine("Jogo da Forca");
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine("Palavra secreata " + palavrasecreta);
-                Console.WriteLine("---------------------------------------");
 
-                //obtem apenas 1 caracter
 
-                Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()[0];
-                Console.WriteLine(chute);
+                int quantidadedeerros = 0;
+                bool jogadorenforcou = false;
+                bool jogadoracertou = false;
+
+
+                do
+                {
+                    string dicadapalavra = String.Join("" ,letrasencontradas);
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Jogo da Forca");
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Palavra secreata " + dicadapalavra);
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Quantiadade de erros: " + quantidadedeerros);
+                    Console.WriteLine("---------------------------------------");
+
+
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0];
+
+                    bool Letrafoiencontrada = false;
+
+                    for ( int contador = 0; contador < palavrasecreta.Length; contador++)
+                    {
+                        char letraatual = palavrasecreta[contador];
+                        
+                        if (chute == letraatual)
+                        {
+                            letrasencontradas[contador] = letraatual;
+                            Letrafoiencontrada = true;
+                        }
+                    }
+                    if (Letrafoiencontrada == false)
+                        quantidadedeerros++;
+                    
+                    
+                    dicadapalavra = String.Join(" ", letrasencontradas);
+
+                    jogadoracertou = dicadapalavra == palavrasecreta;
+
+                    jogadorenforcou = quantidadedeerros > 5;
+
+                    if (jogadoracertou)
+                    {
+                        Console.WriteLine("---------------------------------------");
+                        Console.WriteLine("você acertou a palavra secreta era" + palavrasecreta);
+                        Console.WriteLine("---------------------------------------");
+                    }
+                    else if (jogadorenforcou)
+                    {
+                        Console.WriteLine("---------------------------------------");
+                        Console.WriteLine("Que azar, tente novamente");
+                        Console.WriteLine("---------------------------------------");
+                    }
+                } while (jogadoracertou == false && jogadorenforcou == false);   
+
                 Console.ReadLine();
 
-
-
-
-
             }
-            
+
         }
     }
 }
