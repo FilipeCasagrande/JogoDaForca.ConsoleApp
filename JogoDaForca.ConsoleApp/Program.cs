88ca20b1b21151a3022c8f3f60d47a47958f26cc
Filipe <Filipecasagrande1@gmail.com>
2025-03-26 23:19:50 -0300

    implementado sistema de palavras aleatorias random

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -7,14 +7,20 @@
         {
             while (true)
             {
-                string palavrasecreta = "uva";
+                string[] palavrasecreta = { "abacate","abacaxi","acerola", "acai", "araca","goiaba","maca"};
+
+                Random random = new Random();
+
+                int indice = random.Next(palavrasecreta.Length);
+
+                string palavraescolhida = palavrasecreta[indice];
 
                 char[] letrasencontradas = new char[palavrasecreta.Length];
 
-                for (int caractere = 0; caractere < letrasencontradas.Length; caractere++)
-                {
+                for (int caractere = 0; caractere < palavraescolhida.Length; caractere++)
+                
                     letrasencontradas[caractere] = '_';
-                }
+             
 
                 int quantidadedeerros = 0;
                 bool jogadorenforcou = false;
@@ -22,7 +28,7 @@
 
                 do
                 {
-                    string dicadapalavra = String.Join(" " ,letrasencontradas);
+                    string dicadapalavra = String.Join("", letrasencontradas);
                     Console.Clear();
                     Console.WriteLine("---------------------------------------");
                     Console.WriteLine("Jogo da Forca");
@@ -43,7 +49,7 @@
                         Console.WriteLine(" |                  ");
                         Console.WriteLine("_|____              ");
                     }
-                    else if(quantidadedeerros == 1)
+                    else if (quantidadedeerros == 1)
                     {
                         Console.WriteLine(" ___________        ");
                         Console.WriteLine(" |/        |        ");
@@ -106,14 +112,14 @@
 
                     Console.Write("Digite uma letra: ");
                     char chute = Console.ReadLine()[0];
-                    
+
 
                     bool Letrafoiencontrada = false;
 
-                    for ( int contador = 0; contador < palavrasecreta.Length; contador++)
+                    for (int contador = 0; contador < palavraescolhida.Length; contador++)
                     {
-                        char letraatual = palavrasecreta[contador];
-                        
+                        char letraatual = palavraescolhida[contador];
+
                         if (chute == letraatual)
                         {
                             letrasencontradas[contador] = letraatual;
@@ -122,18 +128,18 @@
                     }
                     if (Letrafoiencontrada == false)
                         quantidadedeerros++;
-                    
-                    
-                    dicadapalavra = String.Join("" , letrasencontradas);
 
-                    jogadoracertou = dicadapalavra == palavrasecreta;
+
+                    dicadapalavra = String.Join("", letrasencontradas);
+
+                    jogadoracertou = dicadapalavra == palavraescolhida;
 
                     jogadorenforcou = quantidadedeerros > 5;
 
                     if (jogadoracertou)
                     {
                         Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("você acertou a palavra secreta era: " + palavrasecreta);
+                        Console.WriteLine("você acertou a palavra secreta era: " + palavrasecreta[indice]);
                         Console.WriteLine("---------------------------------------");
                     }
                     else if (jogadorenforcou)
@@ -142,9 +148,14 @@
                         Console.WriteLine("Que azar, tente novamente");
                         Console.WriteLine("---------------------------------------");
                     }
-                } while (jogadoracertou == false && jogadorenforcou == false);   
+                } while (jogadoracertou == false && jogadorenforcou == false);
 
-                Console.ReadLine();
+                Console.WriteLine("Deseja continuar o jogo [S/N]?");
+                string opcaocontinuar = Console.ReadLine();
+                if (opcaocontinuar == "n")
+                {
+                    break;
+                }
 
             }
 
